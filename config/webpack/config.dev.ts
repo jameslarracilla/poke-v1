e31bf7e073b253be13eccc6a0cb44a250445a75f
José Larracilla <larracillajose.24@gmail.com>
2023-01-26 13:26:46 -0600

    Refactor webpack configs

--- config/webpack/config.dev.ts
+++ config/webpack/config.dev.ts
@@ -1,30 +1,38 @@
 import { Configuration } from 'webpack';
-import { configCommonWebpack, ROOT_DIST } from './config.common';
-import { Configuration as DevServerConfig } from 'webpack-dev-server';
+import {} from 'webpack-dev-server';
+import path from 'path';
+import merge from 'webpack-merge';
+import CommonWepackConfig from './config.common';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { apiMockerServer } from './apiMocker/config-serve';
 
-interface WebpackConfigDev extends Configuration {
-  devServer?: DevServerConfig;
-}
+const ROOT_DIR = path.resolve(__dirname, '../../');
+const DIST_DIR = `${ROOT_DIR}/dist`;
 
-const devConfigWebpack = (env: any, arg: any): WebpackConfigDev => {
-  return {
-    ...configCommonWebpack(env, arg),
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-      static: ROOT_DIST,
-      port: 3000,
-      open: true,
-      liveReload: true,
-      hot: true,
-      historyApiFallback: true,
-      setupMiddlewares: (middlewares, devServer) => {
-        apiMockerServer(devServer);
-        return middlewares;
-      },
+const DevConfig: Configuration = {
+  mode: 'development',
+  output: {
+    path: DIST_DIR,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: `${ROOT_DIR}/public/index.html`,
+      inject: true,
+    }),
+  ],
+  devtool: 'source-map',
+  devServer: {
+    static: DIST_DIR,
+    port: 3000,
+    liveReload: true,
+    open: true,
+    historyApiFallback: true,
+    setupMiddlewares: (middlewares, devServer) => {
+      apiMockerServer(devServer);
+      return middlewares;
     },
-  };
+  },
 };
 
-export default devConfigWebpack;
+export default merge(CommonWepackConfig, DevConfig);
--- config/webpack/config.prod.ts
+++ config/webpack/config.prod.ts
@@ -1,20 +1,25 @@
-import webpack from "webpack";
-import TerserExtractPlugin from "terser-webpack-plugin";
-import { configCommonWebpack } from "./config.common";
+import { Configuration } from 'webpack';
+import path from 'path';
+import merge from 'webpack-merge';
+import CommonWepackConfig from './config.common';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const prodConfigWebpack = (env: any, arg: any): webpack.Configuration => {
-  return {
-    ...configCommonWebpack(env, arg),
-    mode: "production",
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserExtractPlugin({
-          extractComments: true,
-        }),
-      ],
-    },
-  };
+const ROOT_DIR = path.resolve(__dirname, '../../');
+const BUILD_DIR = `${ROOT_DIR}/build`;
+
+const ProdConfig: Configuration = {
+  mode: 'production',
+  output: {
+    path: BUILD_DIR,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: `${ROOT_DIR}/public/index.html`,
+      inject: true,
+      minify: true,
+    }),
+  ],
 };
 
-export default prodConfigWebpack;
+export default merge(CommonWepackConfig, ProdConfig);
